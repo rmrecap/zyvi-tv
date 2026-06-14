5262071887ff5efbc0db8ecc2d837dd94f83b44f
Zyvi TV Dev <dev@zyvi.tv>
2026-06-14 22:01:52 +0000

    fix: set compileSdk 36 via plugins.withId for all android subprojects

--- zyvi_tv/android/build.gradle.kts
+++ zyvi_tv/android/build.gradle.kts
@@ -15,8 +15,17 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-tasks.withType<com.android.build.gradle.tasks.CheckAarMetadataWorkAction>().configureEach {
-    compileSdk = 36
+subprojects {
+    plugins.withId("com.android.application") {
+        configure<com.android.build.gradle.AppExtension> {
+            compileSdk = 36
+        }
+    }
+    plugins.withId("com.android.library") {
+        configure<com.android.build.gradle.LibraryExtension> {
+            compileSdk = 36
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
