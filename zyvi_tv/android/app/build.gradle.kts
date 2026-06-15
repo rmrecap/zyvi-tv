2eaa6befaaedce84ad37a78717fad28d8b3af405
Zyvi TV Dev <dev@zyvi.tv>
2026-06-15 21:59:11 +0000

    feat: update Kotlin plugin configuration and adjust gradle properties for improved compatibility

--- zyvi_tv/android/app/build.gradle.kts
+++ zyvi_tv/android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -27,6 +28,12 @@ android {
         release {
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
