93536fc2d47c7d5f08e5a86028b0366102fe8811
Zyvi TV Dev <dev@zyvi.tv>
2026-06-15 21:54:51 +0000

    refactor: clean up Kotlin configuration and update gradle properties for better compatibility

--- zyvi_tv/android/app/build.gradle.kts
+++ zyvi_tv/android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -17,10 +16,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.zyvi_tv"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
@@ -29,16 +25,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
-    }
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
--- zyvi_tv/android/settings.gradle.kts
+++ zyvi_tv/android/settings.gradle.kts
@@ -20,7 +20,6 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
